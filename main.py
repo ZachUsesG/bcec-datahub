797ce6cc9f7126ffb31ff192fe6cde1a43f3bf44
Zacharia Makari <zacharia.makari@berkeley.edu>
2025-12-24 22:20:10 -0800

    backend save debug2

--- main.py
+++ main.py
@@ -21,7 +21,7 @@ app.add_middleware(
     allow_origins=[
         "https://bcec-datahub.up.railway.app",
         "http://localhost:5173",
-        "http://localhost:3000",
+        "http://127.0.0.1:5173",
     ],
     allow_credentials=True,
     allow_methods=["*"],
